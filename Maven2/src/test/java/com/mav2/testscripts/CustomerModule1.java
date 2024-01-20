9d4d1641ce81f5fb8251d4829bf102c47e7685df
Thejashwini <Thejashwini@DESKTOP-MIFDNCE>
2024-01-20 10:59:39 +0530

    CreateCustomer

---
+++ Maven2/src/test/java/com/mav2/testscripts/CustomerModule1.java
@@ -0,0 +1,12 @@
+package com.mav2.testscripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CustomerModule1 {
+	@Test
+	public void createcustomer() {
+		Reporter.log("createcustomer",true);
+		
+	}
+}
